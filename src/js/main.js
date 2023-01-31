c2dd48419ca395db84d7e58b0470c38650686881
Vedar006 <islamparchev006@gmail.com>
2023-01-31 16:10:36 +0300

    services-pricing

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,7 @@ import Swiper, { Navigation, Pagination } from 'swiper';
 
 
 const portSlider = document.querySelector('.portfolio-section__items');
+const relatedSlider = document.querySelector('.related-projects__items');
 const bodyStyles = window.getComputedStyle(document.body);
 const gap = parseInt(bodyStyles.getPropertyValue('--grid-gap'));
 class GraphAccordion {
@@ -124,7 +125,64 @@ if(portSlider) {
     nextNextActiveSlide.classList.add('slider-visible');
   });
 }
+if(relatedSlider) {
+  const relatedProjSlider = new Swiper(relatedSlider, {
+    slidesPerView: 3,
+    spaceBetween:gap,
+    on: {
+      init:function () {
+        console.log('swiper init');
 
+        const activeSlide = relatedSlider.querySelector('.swiper-slide-active');
+
+        const nextActiveSlide = activeSlide.nextElementSibling;
+        const nextNextActiveSlide = nextActiveSlide.nextElementSibling;
+
+        activeSlide.classList.add('slider-visible');
+        nextActiveSlide.classList.add('slider-visible');
+        nextNextActiveSlide.classList.add('slider-visible');
+
+      }
+    },
+    navigation: {
+      nextEl:'.related-projects__next',
+      prevEl:'.related-projects__prev'
+    },
+  });
+
+  document.querySelector('.related-projects__prev').addEventListener('click',()=> {
+
+    const activeSlide = relatedSlider.querySelector('.swiper-slide-next');
+
+    document.querySelectorAll('.related-projects__items .swiper-slide').forEach(el => {
+      el.classList.remove('slider-visible');
+    });
+
+
+    if(activeSlide.previousElementSibling) {
+      const nextActiveSlide = activeSlide.previousElementSibling;
+      activeSlide.classList.add('slider-visible');
+      nextActiveSlide.classList.add('slider-visible');
+      activeSlide.nextElementSibling.classList.add('slider-visible');
+    }
+
+  });
+
+  document.querySelector('.related-projects__next').addEventListener('click',()=> {
+    const activeSlide = relatedSlider.querySelector('.swiper-slide-active');
+
+    const nextActiveSlide = activeSlide.nextElementSibling;
+    const nextNextActiveSlide = nextActiveSlide.nextElementSibling;
+
+    document.querySelectorAll('.related-projects__items .swiper-slide').forEach(el => {
+      el.classList.remove('slider-visible');
+    });
+
+    activeSlide.classList.add('slider-visible');
+    nextActiveSlide.classList.add('slider-visible');
+    nextNextActiveSlide.classList.add('slider-visible');
+  });
+}
 
 
 
