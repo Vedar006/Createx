bd7f668089a3cc46d93507c7e86bed51195eb73e
Vedar006 <islamparchev006@gmail.com>
2023-02-01 20:46:17 +0300

    work-start

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,20 @@ import './_functions';
 import './_components';
 import Swiper, { Navigation, Pagination } from 'swiper';
 
+const portfolioTabsNav = document.querySelector('.portfolio-tabs-nav');
+const portfolioTabsItems = document.querySelectorAll('.portfolio-tabs__item');
+const portfolioTabsItemsVisible = document.querySelectorAll('.portfolio-tabs__item--visible');
+const portfolioTabsBtns = document.querySelectorAll('.portfolio-tabs-nav__btn');
+const loadMore = document.querySelector('.portfolio-more');
+
+const isLoadMoreNeeded = (selector) => {
+  if(selector.length <= 9) {
+    loadMore.style.display = 'none';
+
+  }else {
+    loadMore.style.display = 'inline-flex';
+  }
+};
 
 const portSlider = document.querySelector('.portfolio-section__items');
 const relatedSlider = document.querySelector('.related-projects__items');
@@ -246,3 +260,73 @@ circles.forEach(el => {
   }
 
 
+
+// Tabs
+  portfolioTabsNav.addEventListener('click',(e)=> {
+
+    const target = e.target;
+    if(target.classList.contains('portfolio-tabs-nav__btn')) {
+
+      const path = target.dataset.path;
+
+      portfolioTabsBtns.forEach((el)=> {
+
+        el.classList.remove('portfolio-tabs-nav__btn--active');
+      });
+      target.classList.add('portfolio-tabs-nav__btn--active');
+      portfolioTabsItems.forEach((el) =>
+      {
+
+        el.classList.remove('portfolio-tabs__item--visible');
+        el.classList.remove('portfolio-tabs__item--visible-more');
+      });
+      document.querySelectorAll(`[data-target="${path}"]`).forEach((el) =>
+       {
+        el.closest('.portfolio-tabs__item').classList.add('portfolio-tabs__item--visible');
+      });
+
+
+      isLoadMoreNeeded(document.querySelectorAll(`[data-target="${path}"]`));
+
+      if(path == 'all') {
+
+        portfolioTabsItems.forEach((el) =>
+        {
+
+          el.classList.add('portfolio-tabs__item--visible');
+
+        });
+        isLoadMoreNeeded(document.querySelectorAll('.portfolio-tabs__item--visible'));
+
+      }
+    }
+  });
+
+  isLoadMoreNeeded(portfolioTabsItemsVisible);
+
+
+loadMore.addEventListener('click',(e)=> {
+
+  const visibleItems = document.querySelectorAll('.portfolio-tabs__item--visible');
+
+  const path = document.querySelector('.portfolio-tabs-nav__btn--active').dataset.path;
+  if(path == 'all') {
+
+    portfolioTabsItems.forEach(el => {
+
+      el.classList.add('portfolio-tabs__item--visible-more');
+
+      loadMore.style.display = 'none';
+    });
+
+
+
+  }else {
+
+    document.querySelectorAll(`[data-target="${path}"]`).forEach(el => {
+
+      el.closest('.portfolio-tabs__item').classList.add('portfolio-tabs__item--visible-more');
+    });
+    loadMore.style.display = 'none';
+  }
+});
